9383c239f22328b45114d6f14b44ba079af0818e
Henry.Kim <hyeongjong90@gmail.com>
2021-01-26 22:18:08 +0900

    프로젝트 생성

---
+++ routes/page.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user; // passport 에서 넣어준 사용자 객체
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', {title: '내 정보 - NodeBird'});
+});
+
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', {title: '회원가입 - NodeBird'});
+});
+
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+module.exports = router;
